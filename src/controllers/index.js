8642ddb77fef96d47439779e0d37e9e026d68871
KhangNguyen0105 <khangnguyen.010598@gmail.com>
2025-10-15 07:40:35 +0700

    new branch: KhangNM

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,7 @@ const UserController = require("./user.controller");
 const OrganizationController = require("./organization.controller");
 const ProductController = require("./product.controller");
 
+
 module.exports = {
   AuthController,
   UserController,
